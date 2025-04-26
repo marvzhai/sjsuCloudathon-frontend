b61dc5a37b47e06b0a2905946bf6447d2821fc3b
Marvin <marvin.zhai@sjsu.edu>
2025-04-26 12:34:58 -0700

    set up next proj instead

---
+++ src/app/page.js
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline text-red-600">Hello World</h1>
+    </div>
+  );
+}
